13f25b5c6f1a280e020fa6f63514186ec8d7a51a
angelica <angelica.milagros167@gmail.com>
2026-07-11 17:53:39 +0000

    Se corrigió error en proceso 3 y se agregó while para que permita actualizar otro lote

--- Sistema.py
+++ Sistema.py
@@ -99,40 +99,44 @@ def consultar_trama():
       
 def actualizar_estado():
     print("\nACTUALIZAR ESTADO")
-    codigo_buscado = input("Ingrese código de lote:")
+    seguir = "s"
     
-    indice = -1
-    for i in range(len(codigos_lote)):
-        if codigos_lote[i] == codigo_buscado:
-            indice = i
-    #probar si funciona        
-    if indice == -1:
-        print("\nTrama no existe")
-        return'
+    while seguir == "s":
+        codigo_buscado = input("Ingrese código de lote:")
         
-    print("Total de expedientes:",totales_expedientes[indice])
-    rechazados = int(input("Cantidad de expedientes rechazados:"))
-    
-    #probar calculo
-    aceptados = totales_expedientes[indice] - rechazados
-    
-    #probar si se actualizan valores
-    rechazados_lista[indice] = rechazados
-    aceptados_lista[indice] = aceptados
-    
-    necesita_ticket = input("¿La trama requiere ticket? (s/n)").lower()
-    
-    if necesita_ticket = "s":
-        estados[indice] = "TICKET PENDIENTE"
-    else:
-        estados[indice]= "PROCESADA"
-    
-    print("\nActualizacion guardada correctamente")
-    print("Expedientes aceptados:",aceptados)
-    print("Expedientes rechazados:",rechazados)
-    print("Estado actualizado:",estados[indice])
-    #pendiente ver si se implementa bucle para actualizar, revisar diagrama
-    
+        
+        indice = -1
+        for i in range(len(codigos_lote)):
+            if codigos_lote[i] == codigo_buscado:
+                indice = i
+        #probar si funciona        
+        if indice == -1:
+            print("\nTrama no existe")
+            return
+            
+        print("Total de expedientes:",totales_expedientes[indice])
+        rechazados = int(input("Cantidad de expedientes rechazados:"))
+        
+        #probar calculo
+        aceptados = totales_expedientes[indice] - rechazados
+        
+        #probar si se actualizan valores
+        rechazados_lista[indice] = rechazados
+        aceptados_lista[indice] = aceptados
+        
+        necesita_ticket = input("¿La trama requiere ticket? (s/n)").lower()
+        
+        if necesita_ticket == "s":
+            estados[indice] = "TICKET PENDIENTE"
+        else:
+            estados[indice]= "PROCESADA"
+        
+        print("\nActualizacion guardada correctamente")
+        print("Expedientes aceptados:",aceptados)
+        print("Expedientes rechazados:",rechazados)
+        print("Estado actualizado:",estados[indice])
+    #pendiente corregir diagrama proceso 3
+        seguir = input("\n¿Desea actualizar otra trama? (s/n)").lower()
 
 def registrar_ticket():
     #proceso 4
